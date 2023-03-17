f3f32da1f9404f23d466f73c597b0a428715ea20
Hai Ly SJSU <haily2221@gmail.com>
2023-03-17 16:48:39 -0700

    Added homepage template

--- mangadb/src/components/Homepage.js
+++ mangadb/src/components/Homepage.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import "../css/homepage.css"
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div class="grid-container">
+        <div class="item1">Header</div>
+        <div class="item2">Menu</div>
+        <div class="item3">Main</div>  
+        <div class="item5">Footer</div>
+    </div>
   )
 }
 
