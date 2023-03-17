84ae9d91afb0ae1b52b5bdb7a2b34f645269792b
Hai Ly SJSU <haily2221@gmail.com>
2023-03-17 16:26:48 -0700

    Created Links

--- mangadb/src/App.js
+++ mangadb/src/App.js
@@ -1,13 +1,22 @@
 import Login from "./components/Login";
 import Register from "./components/Register";
+import { BrowserRouter, Routes, Route } from 'react-router-dom' 
+import Homepage from "./components/Homepage";
 
 
 function App() {
   //hi this is Joshua
   return (
-    <div>
-      <Login/>
-    </div>
+      <BrowserRouter>
+        <a href="/">Home page</a><br/>
+        <a href="/login">Login</a><br/>
+        <a href="/register">Register</a>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
