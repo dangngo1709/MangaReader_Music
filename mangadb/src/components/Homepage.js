9df4bf9ba307900fc4fc89f4dd7229267299c7e9
Hai Ly SJSU <haily2221@gmail.com>
2023-03-17 21:50:03 -0700

    Update components

--- mangadb/src/components/Homepage.js
+++ mangadb/src/components/Homepage.js
@@ -2,11 +2,24 @@ import React from 'react'
 import "../css/homepage.css"
 const Homepage = () => {
   return (
-    <div class="grid-container">
-        <div class="item1">Header</div>
-        <div class="item2">Menu</div>
-        <div class="item3">Main</div>  
-        <div class="item5">Footer</div>
+    <div className="grid-container">
+        <div className="item1">Header</div>
+        <div className="item2">Menu</div>
+        <div className="item3">Main</div>  
+        <div className="item5">
+            <h3 id="cont-read">Continue Reading</h3>
+            <div className="book-row">
+                <div className="book-block">Title</div>
+                <div className="book-block">Title</div>
+                <div className="book-block">Title</div>
+                <div className="book-block">Title</div>
+                <div className="book-block">Title</div>
+                <div className="book-block">Title</div>
+                <div className="book-block">Title</div>
+                <div className="book-block">Title</div>
+                <div className="book-block">Title</div>
+            </div>
+        </div>
     </div>
   )
 }
