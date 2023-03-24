10b011b4cbb4d66404dc059ace0e54cd8fc181a6
dangngo1709 <112128470+dangngo1709@users.noreply.github.com>
2023-03-24 12:19:40 -0700

    creating menu button

--- mangadb/src/components/Homepage.js
+++ mangadb/src/components/Homepage.js
@@ -1,28 +1,48 @@
-import React from 'react'
-import "../css/homepage.css"
+import React, { useState } from 'react';
+import "../css/homepage.css";
+
 const Homepage = () => {
+  const [isMenuExpanded, setIsMenuExpanded] = useState(false);
+  const [isAccountExpanded, setIsAccountExpanded] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuExpanded(!isMenuExpanded);
+  };
+
+  const handleAccountClick = () => {
+    setIsAccountExpanded(!isAccountExpanded);
+  };
+
   return (
     <div class="grid-container">
-        <div class="item1">Header</div>
-        <div class="item2">
-          <div class= "menu">
-              <div>Menu</div>  
-              <div class = "Home">
-                Home
-                </div>
-              <div class ="RankingSystem"> Ranking System</div>
-          </div>
-
-              <div class = "account"> Account
-                <div class ="profile">Profile</div>
-                <div class ="bookmarked">Bookmarked</div>
-                <div class ="ReadingHistory">Reading History</div>
-              </div>
+      <div class="item1">Header</div>
+      <div class="item2">
+        <div class="menu">
+          <button onClick={handleMenuClick} className="MenuButton">
+            Menu
+          </button>
+          {isMenuExpanded && (
+            <div className="submenu">
+              <button className="HomeButton">Home</button>
+              <button className="RankingButton">Ranking</button>
+            </div>
+          )}
+          <button onClick={handleAccountClick} className="AccountButton">
+            Account
+          </button>
+          {isAccountExpanded && (
+            <div className="submenu">
+              <button className="ProfileButton">Profile</button>
+              <button className="MyListButton">My List</button>
+              <button className="SettingButton">Settings</button>
+            </div>
+          )}
         </div>
-        <div class="item3">Main</div>  
-        <div class="item5">Footer</div>
+      </div>
+      <div class="item3">Main</div>
+      <div class="item5">Footer</div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
